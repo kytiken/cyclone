9a294bc16810f5721596d9c0ef7cf91dc4c91e5d
kytiken <kytiken@gmail.com>
2018-07-15 06:42:55 +0900

    add material-ui template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-const App = () => (<div><p>hello</p></div>)
+const App = () => (
+  <div>
+    <AppBar position='static' color='default'>
+      <Toolbar>
+        <Typography variant='title' color='inherit'>
+          Cyclone
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
 
 export default App
