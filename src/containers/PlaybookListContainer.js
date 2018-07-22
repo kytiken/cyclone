181ad091c0132888188b5ec4374ec5163d161abf
kytiken <kytiken@gmail.com>
2018-07-22 12:47:41 +0900

    add playbook records

--- src/containers/PlaybookListContainer.js
+++ src/containers/PlaybookListContainer.js
@@ -6,15 +6,71 @@ const initializePlaybooks = (dispatch) => {
   return () => {
     var PouchDB = require('pouchdb')
     var db = new PouchDB('db')
+
+    var playbookRecords = [
+      {
+        'name': 'essential brew package',
+        'tasks': [{
+          name: 'Install homebrew packages',
+          homebrew: 'name={{ item }}',
+          with_items: [
+            'git',
+            'openssl',
+            'wget',
+            'curl',
+            'fish',
+            'graphviz',
+            'tig',
+            'tmux',
+            'elixir',
+            'haskell-stack',
+            'neovim/neovim/neovim',
+            'awscli',
+            'rmtrash',
+            'exa',
+            'the_silver_searcher',
+            'rbenv',
+            'peco',
+            'dry'
+          ]
+        }]
+      },
+      {
+        'name': 'cask',
+        'tasks': [{
+          'name': 'Install Homebrew cask packages',
+          'homebrew_cask': 'name={{ item }}',
+          'with_items': [
+            'java',
+            'android-studio',
+            'kindle',
+            'vlc',
+            'skype',
+            'docker',
+            'camtwist',
+            'obs',
+            'astah-community',
+            'google-japanese-ime',
+            'iterm2',
+            'google-chrome',
+            'vagrant',
+            'silverlight',
+            'flash-player',
+            'google-cloud-sdk',
+            'sequel-pro',
+            'visual-studio-code',
+            'amazon-drive',
+            'kitematic',
+            'hyper',
+            'spectacle'
+          ]
+        }]
+      }
+    ]
+
     // db.put({
     //   '_id': 'playbooks',
-    //   'records': [
-    //     {
-    //       'name': 'git',
-    //       'type': 'homebrew',
-    //       'task': {}
-    //     }
-    //   ]
+    //   'records': playbookRecords
     // })
     db.get('playbooks').then((doc) => {
       doc.records.forEach((playbook) => {
@@ -26,7 +82,8 @@ const initializePlaybooks = (dispatch) => {
 
 const mapStateToProps = (state) => {
   return {
-    playbooks: state.playbooks
+    playbooks: state.playbooks,
+    password: state.password
   }
 }
 
