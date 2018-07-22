ebfdad31263b6e80a284887f9caa21a5a1e7213e
kytiken <kytiken@gmail.com>
2018-07-22 14:59:04 +0900

    remove unuse mapStateToProps

--- src/containers/PasswordFieldContainer.js
+++ src/containers/PasswordFieldContainer.js
@@ -3,13 +3,8 @@ import { bindActionCreators } from 'redux'
 import actions from '../actions/password'
 import PasswordField from '../components/PasswordField'
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
-
 const mapDispatchToProps = (dispatch) => {
   return { actions: bindActionCreators(actions, dispatch) }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PasswordField)
+export default connect(null, mapDispatchToProps)(PasswordField)
